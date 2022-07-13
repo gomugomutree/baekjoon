607349d9f5f69fe3d42697bc4321ac3949d0ff78
gomugomutree <gomugomutree@gmail.com>
2022-07-13 19:36:21 +0900

    feat : programmers 42862

---
+++ coding test/programmers_42862.py
@@ -0,0 +1,33 @@
+def solution(n, lost, reserve):
+    answer = 0
+    # 전체 학생번호 : index
+    # 체육복 보유수 : value
+    student = [1] * (n+2)
+    # 0번 학생과 여분 학생번호는 0
+    student[0] = 0
+    student[-1] = 0
+    # print(student)
+    
+    # 도난당한 학생의 채육복 -1
+    for i in lost:
+        student[i] -= 1
+        
+    # 여벌 채육복이 있는 학생 채육복 +1
+    for i in reserve:
+        student[i] += 1
+        
+    # 빌려주기
+    for i in range(1, len(student)-1):
+        if student[i] == 0 and student[i-1] == 2:
+            student[i] = 1
+            student[i-1] = 1
+            
+        elif student[i] ==0 and student[i+1] ==2:
+            student[i] = 1
+            student[i+1] = 1
+
+    for i in range(1, len(student)-1):
+        if student[i] >= 1:
+            answer += 1
+            
+    return answer
