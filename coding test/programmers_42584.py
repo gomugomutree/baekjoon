98c5b8590c1f465452c8e43a5ead3becb50d42c3
gomugomutree <gomugomutree@gmail.com>
2022-07-29 22:38:01 +0900

    feat : complete

--- coding test/programmers_42584.py
+++ coding test/programmers_42584.py
@@ -1,26 +1,26 @@
 def solution(prices):
-    answer = []
+    # 반환값도 같은 길이로 만든다.
+    answer = [0] * len(prices)
     
-    for  _ in range(len(prices)):
-        # 리스트 첫 숫자 
-        first = prices.pop(0)
+    for idx in range(len(prices)):
+        # 비교할 숫자를 넣는다.
+        first = prices[idx] 
         
         # 시간 체크
         check = 0
         
         # 첫 숫자와 비교
-        for i in range(len(prices)):
+        for i in range(idx+1, len(prices)):
             if first > prices[i]: # 가격 떨어지면
-                answer.append(i+1)
-                check = 0
+                answer[idx] = i - idx # 시간차이(index 차이) 넣기
+                check = 0 
                 break
             else:
                 check += 1 # 초 진행
         
         # 끝까지 가격이 안 떨어지면 
         if check >= 1:
-            answer.append(check)
-    # 마지막 가격 
-    answer.append(0)
-
+            # 비교하는 숫자랑 반환값이랑 index가 같다
+            answer[idx] = check 
+            
     return answer
