69d5c5f196164a0a276adaec802e21273d4e6692
gomugomutree <gomugomutree@gmail.com>
2022-08-24 21:37:39 +0900

    43165

---
+++ coding test/programmers_43165.py
@@ -0,0 +1,10 @@
+def solution(numbers, target):
+    if not numbers and target == 0:
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution(numbers=numbers[1:], target = target-numbers[0]) + solution(numbers=numbers[1:], target = target+numbers[0])
+
+
+print(solution([1, 1, 1, 1, 1], 3))
